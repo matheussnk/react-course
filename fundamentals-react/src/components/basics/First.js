ddb2665a29895477b3650dbc46456089a6363e2b
Matheus <matheus.lima.3@globalhitss.com.br>
2021-10-11 12:02:17 -0300

    React course

---
+++ fundamentals-react/src/components/basics/First.js
@@ -0,0 +1,3 @@
+export default function First(params) {
+    return ('First Component')
+}
---
+++ fundamentals-react/src/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import First from './components/basics/First'
+
+ReactDOM.render(
+    <div>
+        <First></First>
+    </div>,
+    document.getElementById('root')
+)
