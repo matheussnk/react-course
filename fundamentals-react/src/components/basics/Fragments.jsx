4c858f1aa03fda7b494ce54838cf3869f818dc13
Matheus <matheus.lima.3@globalhitss.com.br>
2021-10-11 13:58:12 -0300

    Using react.fragment to avoid conflicts or errors

---
+++ fundamentals-react/src/components/basics/Fragments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Fragments (props){
+    return (
+        <React.Fragment>
+            <h2> Fragments </h2>
+                <p>Caution Error !</p>
+        </React.Fragment>
+    )
+}
--- fundamentals-react/src/index.js
+++ fundamentals-react/src/index.js
@@ -4,13 +4,16 @@ import React from 'react'
 
 import First from './components/basics/First'
 import WithParameter from'./components/basics/WithParameter'
+import Fragments from'./components/basics/Fragments'
+
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <First></First>
         <WithParameter title="Grades" student="Mark Doe" grades='9.0'></WithParameter>
         <WithParameter title="Grades" student="Jhon  Doe" grades='7.9'></WithParameter>
         <WithParameter title="Grades" student="Susan Doe" grades='8.0'></WithParameter>
+        <Fragments></Fragments>
 
     </div>,
     document.getElementById('root')
